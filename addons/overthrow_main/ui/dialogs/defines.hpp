245f7a71659cc8d3b16c7563342b085376af3146
Nifnat <nifnat@gmail.com>
2020-04-18 20:55:35 +0100

    Update defines.hpp
    Textbox abstract and imporved scrollbar.

--- addons/overthrow_main/ui/dialogs/defines.hpp
+++ addons/overthrow_main/ui/dialogs/defines.hpp
@@ -107,6 +107,74 @@ class RscControlsGroup;
 ///////////////////////////////////////////////////////////////////////////
 /// Base Classes
 ///////////////////////////////////////////////////////////////////////////
+class RscEditOverthrow
+{
+	deletable = 0;
+	fade = 0;
+	access = 0;
+	type = 2;
+	x = 0;
+	y = 0;
+	h = 0.04;
+	w = 0.2;
+	colorBackground[] = 
+	{
+		0,
+		0,
+		0,
+		1
+	};
+	colorText[] = 
+	{
+		0.95,
+		0.95,
+		0.95,
+		1
+	};
+	colorDisabled[] = 
+	{
+		1,
+		1,
+		1,
+		0.25
+	};
+	colorSelection[] = 
+	{
+		"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.69])",
+		"(profilenamespace getvariable ['GUI_BCG_RGB_G',0.75])",
+		"(profilenamespace getvariable ['GUI_BCG_RGB_B',0.5])",
+		1
+	};
+	autocomplete = "";
+	text = "";
+	size = 0.2;
+	style = "0x00 + 0x40";
+	font = "PuristaMedium";
+	shadow = 2;
+	sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
+	canModify = 1;
+	tooltipColorText[] = 
+	{
+		1,
+		1,
+		1,
+		1
+	};
+	tooltipColorBox[] = 
+	{
+		1,
+		1,
+		1,
+		1
+	};
+	tooltipColorShade[] = 
+	{
+		0,
+		0,
+		0,
+		0.65
+	};
+};
 class RscOverthrowText
 {
 	deletable = 0;
@@ -637,12 +705,18 @@ class RscOverthrowListBox
 	};
 	class ListScrollBar
 	{
-		color[] =
-		{
-			1,
-			1,
-			1,
-			1
+		width = 0;
+		height = 0;
+		scrollSpeed = 0.01;
+		arrowEmpty = "\A3\ui_f\data\gui\cfg\scrollbar\arrowEmpty_ca.paa";
+		arrowFull = "\A3\ui_f\data\gui\cfg\scrollbar\arrowFull_ca.paa";
+		border = "\A3\ui_f\data\gui\cfg\scrollbar\border_ca.paa";
+		thumb = "\A3\ui_f\data\gui\cfg\scrollbar\thumb_ca.paa";
+		color[] = {
+		"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.69])",
+		"(profilenamespace getvariable ['GUI_BCG_RGB_G',0.75])",
+		"(profilenamespace getvariable ['GUI_BCG_RGB_B',0.5])",
+		1
 		};
 		autoScrollEnabled = 1;
 	};
