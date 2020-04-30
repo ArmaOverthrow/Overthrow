62f8bf3b1370b191ef55adfdbf2a7023498fa8a7
Triada <tabenkov@yandex.ru>
2020-04-30 23:48:43 +0300

    Update CfgMagazines.hpp
    fix wrong inheritance

--- addons/overthrow_main/CfgMagazines.hpp
+++ addons/overthrow_main/CfgMagazines.hpp
@@ -1,21 +1,25 @@
 class CfgMagazines {
     class DemoCharge_Remote_Mag;
-    class IEDUrbanSmall_Remote_Mag {
+    class IEDUrbanSmall_Remote_Mag : DemoCharge_Remote_Mag
+    {
         ot_craftable = 1;
         ot_craftQuantity = 1;
         ot_craftRecipe = "[[""NLAW_F"",1],[""ACE_Cellphone"",1]]";
     };
-    class IEDLandSmall_Remote_Mag {
+    class IEDLandSmall_Remote_Mag : DemoCharge_Remote_Mag
+    {
         ot_craftable = 1;
         ot_craftQuantity = 1;
         ot_craftRecipe = "[[""NLAW_F"",1],[""ACE_Cellphone"",1]]";
     };
-    class IEDUrbanBig_Remote_Mag {
+    class IEDUrbanBig_Remote_Mag : DemoCharge_Remote_Mag
+    {
         ot_craftable = 1;
         ot_craftQuantity = 1;
         ot_craftRecipe = "[[""NLAW_F"",4],[""ACE_Cellphone"",1]]";
     };
-    class IEDLandBig_Remote_Mag {
+    class IEDLandBig_Remote_Mag : DemoCharge_Remote_Mag
+    {
         ot_craftable = 1;
         ot_craftQuantity = 1;
         ot_craftRecipe = "[[""NLAW_F"",4],[""ACE_Cellphone"",1]]";
